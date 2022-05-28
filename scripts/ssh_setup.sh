8e674de4d681020df821f4637b9bb8a7b5c8bef1
hiroki-0001 <touhu.273@gmail.com>
2022-05-28 14:40:59 +0900

    SSHkey setup script

---
+++ scripts/ssh_setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -euxo pipefail
+
+HOME_DIR="${HOME}"
+cd $HOME_DIR/.ssh
+sudo apt-get install xsel
+ssh-keygen -t ed25519
+cat ~/.ssh/id_ed25519.pub | xsel --clipboard --input
+
+cat <<EOF > aaa
+Host github github.com
+    HostName ssh.github.com
+    IdentityFile ~/.ssh/id_ed25519
+    User git
+    port 443
+    IdentitiesOnly yes  
+EOF
+
+cat << EOS
+=========================================
+SSH Key created success
+Please apply GitHub
+
+https://github.com/
+
+=========================================
+EOS
+
