a23a6cceefc75a334c5e8a96471c29d1da2e641b
hiroki-0001 <touhu.273@gmail.com>
2023-09-06 21:29:56 +0900

    add new setup file
    dockerをインストールするスクリプトを作成しました

---
+++ scripts/docker_setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -euxo pipefail
+
+HOME_DIR="${HOME}"
+
+sudo apt-get -y update
+sudo apt-get install ca-certificates curl gnupg
+
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo \
+    "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+    "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Process to use docker without sudo permission
+sudo gpasswd -a $USER docker
+sudo systemctl restart docker
+sudo chmod 666 /var/run/docker.sock
+
+# test
+docker run hello-world
+
+# reference
+# https://docs.docker.com/engine/install/ubuntu/
